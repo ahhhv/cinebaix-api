6cf4ee45a2d859da00e741530d38437e238ecb45
Rubén <rubenalb90@gmail.com>
2023-10-22 20:51:41 +0200

    Add /movies/:title/imdb endpoint to obtain the IMDb URL, IMDb rating, plot, directors, writers and stars

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const PORT = process.env.PORT || 8000
 const URL = "https://cinebaixtaquilla81.com/"
+const IMDB_BASE_URL = "https://www.imdb.com"
+const IMDB_FIND_URL = "https://www.imdb.com/find/"
 
 const express = require('express')
 const axios = require('axios')
@@ -60,4 +62,83 @@ app.get('/', (req, res) => {
 
             res.json(movies)
         })
+})
+
+app.get('/movies/:title/imdb', async (req, res) => {
+    const title = req.params.title
+
+    const headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+        'Referer': 'https://www.imdb.com/?ref_=nv_home',
+        'Cookie': 'lc-main=es_ES;'
+    }
+
+    const findResponse = await axios.get(`${IMDB_FIND_URL}`,
+        {
+            params: {q: title},
+            headers,
+        },
+    )
+    let html = findResponse.data
+    let $ = cheerio.load(html)
+    const firstMovieUrl = $('section[data-testid="find-results-section-title"] .ipc-metadata-list-summary-item__t')
+        .attr('href');
+
+    const fullFirstMovieUrl = `${IMDB_BASE_URL}${firstMovieUrl}`
+    const movieResponse = await axios.get(fullFirstMovieUrl, { headers })
+    html = movieResponse.data
+    $ = cheerio.load(html)
+    const imdbRating = parseFloat(
+        $('[data-testid="hero-rating-bar__aggregate-rating__score"] > span')
+            .first()
+            .text()
+            .replace(',', '.')
+    )
+    const plot = $('[data-testid="plot-xl"]').text()
+
+    const metadataLabels = $('.ipc-metadata-list .ipc-metadata-list-item__label')
+
+    const directors = []
+    metadataLabels
+        .filter(function (){
+            return $(this).text().trim() === "Dirección";
+        })
+        .next()
+        .first()
+        .each(function () {
+            directors.push($(this).text())
+        })
+
+    const writers = []
+    metadataLabels
+        .filter(function (){
+            return $(this).text().trim() === "Guión";
+        })
+        .next()
+        .first()
+        .find('.ipc-metadata-list-item__list-content-item')
+        .each(function () {
+             writers.push($(this).text())
+        })
+
+    const stars = []
+    metadataLabels
+        .filter(function (){
+            return $(this).text().trim() === "Reparto principal";
+        })
+        .next()
+        .first()
+        .find('.ipc-metadata-list-item__list-content-item')
+        .each(function () {
+            stars.push($(this).text())
+        })
+
+    res.json({
+        url: fullFirstMovieUrl,
+        imdbRating,
+        plot,
+        directors,
+        writers,
+        stars
+    })
 })
